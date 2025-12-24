74a1d96e9f500f189d6f98933b9ea73c39d3fdc7
Goutham-0255 <gouthamf01@gmail.com>
2025-12-24 21:14:10 +0530

    added employee data

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from employee.models import Employee
 
 
 def home(request):
+
+    Employee.objects.all()
     return render(request, 'home.html')
