3fc51c5049f9b2e6cf867c388125a754305a03b9
Goutham-0255 <gouthamf01@gmail.com>
2026-01-02 23:32:16 +0530

    created view for employee

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Employee
+from django.http import Http404, HttpResponse
 
 # Create your views here.
+def employee_detail(request, pk):
+
+    try:
+        employee = Employee.objects.get(pk=pk)
+        return HttpResponse(employee)
+
+    except:
+        raise Http404
